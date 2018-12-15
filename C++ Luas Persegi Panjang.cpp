78c9fe65c6dcc3173eb3afca693f47af547ea85a
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-15 15:17:16 +0700

    Add files via upload

---
+++ C++ Luas Persegi Panjang.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int P,L;
+	
+	cout << "Masukkan Panjang Persegi : ";
+	cin >> P;
+	
+	cout << "Masukkan Luas Persegi : ";
+	cin >> L;
+	
+	cout << "Luas Persegi = " << P*L << "\n";
+	
+	return 0;
+}
